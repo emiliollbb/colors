01be24b3e4227e48d8f14de84068c02d5c0cae6c
emiliollbb <emiliollbb@gmail.com>
2021-12-21 19:31:39 +0100

    Initial code

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "game.hpp"
+#include "jengine.hpp"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	try {
+        Juego *juego = new Juego();
+        juego->run();
+            
+        delete juego;
+    } catch (JEngineException &e) {
+		cout << "Error! " << *e.what() << endl;
+        flush(cout);        
+	}
+    
+    return 0;
+}
